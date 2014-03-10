4e8e3cdca05d1103cae921b50ff526c4112d0c2f
Edgar Zakaryan <zakaryan.edgar@gmail.com>
2014-03-10 09:07:32 +0400

    initialize app by index

--- public_html/assets/js/app.js
+++ public_html/assets/js/app.js
@@ -27,10 +27,12 @@ var app = {
     XMLContent: '',
     
     chapterIndex: 0,
+    questionIndex: 0,
     
-    init: function(chapterIndex) {
+    init: function(chapterIndex, questionIndex) {
         
-        this.chapterIndex = chapterIndex;
+        this.chapterIndex  = parseInt(chapterIndex);
+        this.questionIndex = parseInt(questionIndex);
         
         this.parseXML();
         this.drawHTML(this.getChapter(this.chapterIndex));
@@ -64,6 +66,12 @@ var app = {
             data.questions[i] = {};
             //clean out the comment from the text
             data.questions[i].question = $(this).find('FCQuestion').text().replace(/\[{(.*)}]/, '');
+            
+            if (this.questionIndex === i)
+                data.questions[i].selected = 'selected';
+            
+            console.log(data.questions[i].selected);
+            
             data.questions[i].answer = {};
             data.questions[i].answer.short = {};
             
@@ -72,8 +80,17 @@ var app = {
             });
             
             data.questions[i].answer.long = {};
+            data.questions[i].answer.long.text = {};
             $(this).find('long').find('text').each(function(j) {
-                data.questions[i].answer.long[j] = $(this).text();
+                data.questions[i].answer.long.text[j] = $(this).text();
+            });
+            $(this).find('long').find('SimpleList').find('item').each(function(j) {
+                data.questions[i].answer.long.type = 'simpleList';
+                data.questions[i].answer.long.text[j] = $(this).text();
+            });            
+            $(this).find('long').find('SimpleOList').find('item').each(function(j) {
+                data.questions[i].answer.long.type = 'simpleOList';
+                data.questions[i].answer.long.text[j] = $(this).text();
             });
             
             data.questions[i].answer.bonus = {};
@@ -97,9 +114,13 @@ var app = {
 $(function() {
     var url = purl();
     var chapterIndex = 0;
+    var questionIndex = 0;
     
     if(typeof url.param('chapterIndex') !== 'undefined')
         chapterIndex = url.param('chapterIndex');
     
-    app.init(chapterIndex);
+    if(typeof url.param('questionIndex') !== 'undefined')
+        chapterIndex = url.param('questionIndex');
+    
+    app.init(chapterIndex, questionIndex);
 });
